6b35e095cc8771ff2d8d78baa3761dec25477050
Jolu Izquierdo <joluizquierdo@pm.me>
2019-06-30 21:49:00 +0200

    Code better modularized   - The main file has less code and uses more functions to have everything better modularized   - Improved the commentaries of the code

--- lib/functions.sh
+++ lib/functions.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-#!/bin/bash
+
+# VARS
+
+# Note that they are shared to the whole script, so you can use them
+# inside a function, a reuse them outside that function... 
+SESSION=''
+WINDOW=''
+PANE=0
+
 # FUNCTIONS
+
+#It prints a colored error and aborts execution of the script
+print_error(){
+  echo "$(tput setaf 1)ERROR: $(tput setaf 4) $1$(tput sgr0)"
+  exit 1
+}
 
 # It receives a filename as the first argument and removes all the commentaries
 # (lines preceded by character '#') empty lines and all leading witespaces or
@@ -24,4 +38,34 @@ split() {
   read -ra ARRAY <<< $1
 }
 
+create_session() {
+  tmux new-session -d -c "$1" -s "$2"
+  SESSION="$2"
+  # This line is very important, for more details go to 'create_window' function
+  WINDOW=''
+}
 
+create_window() {
+  # When a session is created, a window is created within it, so later on if
+  # we want to create a window we have to rename it instead of creating a new one.
+  # So if $WINDOW doesn't exist (or is empty), just rename the default window.
+
+  if [ -z "$WINDOW" ]; then
+    COMMAND="cd $1 && ${ARRAY[3]}"
+    tmux rename-window -t "$SESSION" "$2"
+    WINDOW="$2"
+  else
+    tmux new-window -c "$1" -n "$2"  -t "$SESSION"
+    WINDOW="$2"
+  fi
+
+  tmux send-keys -t "$SESSION:$WINDOW" "$COMMAND" ENTER
+}
+
+create_pane() {
+    COMMAND=${ARRAY[2]}
+    SPLIT_OPTION="-${ARRAY[0]:1:1}" # Access the second position of the string contained in ARRAY[0]
+    tmux split-window $SPLIT_OPTION -c "$DIR" -t "${SESSION}:${WINDOW}.${PANE}"
+    PANE=$(expr $PANE + 1)
+    tmux send-keys -t "${SESSION}:${WINDOW}.${PANE}" "$COMMAND" ENTER
+}
--- tmux.sh
+++ tmux.sh
@@ -4,25 +4,19 @@
 
 source lib/functions.sh
 
-# VARS
-SESSION=''
-WINDOW=''
-PANE=0
-
-if [ ! $# -eq 1  ]; then
-  echo "$(tput setaf 1)ERROR: $(tput setaf 4) Invalid number of argument expected 1, given $#$(tput sgr0)"
-  exit 1
+# Manage errors:
+if [ ! $# -eq 1 ]; then
+  print_error "Invalid number of argument expected 1, given $#"
 fi
 
 if [ ! -r $1 ]; then
-  echo "$(tput setaf 1)ERROR: $(tput setaf 4)The file $(tput setaf 2)$1 $(tput setaf 4)don't exists or read permission are not granted$(tput sgr0)"
-  exit 1
+  print_error "The file $1 don't exists or read permission are not granted"
 fi
 
 # Loop through each line of the cleaned file
 clean_file $1 | while read line
 do
-  split "$line"
+  split "$line" # Split line and create ARRAY variable
 
   DIR=${ARRAY[1]}
   NAME=${ARRAY[2]}
@@ -30,28 +24,13 @@ do
 
   case "${ARRAY[0]}" in
     "s")
-      tmux new-session -d -c "$DIR" -s "$NAME"
-      SESSION="$NAME"
-      WINDOW=''
+      create_session "$DIR" "$NAME" "${ARRAY[5]}"
       ;;
     "w")
-      if [ -z "$WINDOW" ]; then # if $WINDOW_NAME doesn't exist, just rename the default window
-        COMMAND="cd $DIR && ${ARRAY[3]}"
-        tmux rename-window -t "$SESSION" "$NAME"
-        WINDOW="$NAME"
-      else
-        tmux new-window -c "$DIR" -n "$NAME"  -t "$SESSION"
-        WINDOW="$NAME"
-      fi
-
-      tmux send-keys -t "$SESSION:$WINDOW" "$COMMAND" ENTER
+      create_window "$DIR" "$NAME"
       ;;
     "pv" | "ph")
-      COMMAND=${ARRAY[2]}
-      SPLIT_OPTION="-${ARRAY[0]:1:1}" # Access the second position of the string contained in ARRAY[0]
-      tmux split-window $SPLIT_OPTION -c "$DIR" -t "${SESSION}:${WINDOW}.${PANE}"
-      PANE=$(expr $PANE + 1)
-      tmux send-keys -t "${SESSION}:${WINDOW}.${PANE}" "$COMMAND" ENTER
+      create_pane
       ;;
   esac
 done
